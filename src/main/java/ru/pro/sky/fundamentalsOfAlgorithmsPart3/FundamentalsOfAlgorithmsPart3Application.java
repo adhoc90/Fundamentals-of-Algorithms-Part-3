8a1d6f7aeff34669560b47f0a79222f7a82cb387
ad_hoc <ad_hoc90@mail.ru>
2023-08-27 23:10:05 +0300

    finish the code write tests

---
+++ src/main/java/ru/pro/sky/fundamentalsOfAlgorithmsPart3/FundamentalsOfAlgorithmsPart3Application.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.fundamentalsOfAlgorithmsPart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPart3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsOfAlgorithmsPart3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pro/sky/fundamentalsOfAlgorithmsPart3/FundamentalsOfAlgorithmsPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.fundamentalsOfAlgorithmsPart3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsOfAlgorithmsPart3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
